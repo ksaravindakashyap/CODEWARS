73545a99f80bf17fb7f9e10818b0ae2044e76fb9
K S ARAVINDA KASHYAP <68753937+ksaravindakashyap@users.noreply.github.com>
2020-11-17 10:48:15 +0530

    Add files via upload
    A code for prime factor decomposition of a given number.

---
+++ primefactdecomposition.py
@@ -0,0 +1,41 @@
+from collections import defaultdict
+
+def prime_factors(n):
+    if n > 0:
+        p = 2
+        a = []
+        while n >= (p**2):
+            if n%p == 0:
+                n = n/p
+                a.append(p)
+                #print(a)
+                continue
+            else:
+                p = p+1
+                continue
+        else:
+            a.append(int(n))
+            #print(a)
+
+        counts = defaultdict(int)
+        for x in a:
+            counts[x] += 1
+
+        print("'", end="")
+        for k,v in counts.items():
+            #print(k,v,end=" ")
+            b = ""
+
+            if v!= 1:
+                b =b.join("("+str(k)+"**"+str(v)+")")
+                print(b,end="")
+
+            elif v == 1:
+                b = b.join("("+str(k)+")")
+                print(b,end="")
+        print("'",end="")
+    else:
+        print("enter valid number")
+
+
+prime_factors(7775460)
